afe9f1d5be873ad2d7fb74d1898efa6d0d04f074
Thrice-J <98914296+Thrice-J@users.noreply.github.com>
2022-03-16 15:35:38 -0400

    Update ForkingHomeworkRfile.R

--- ForkingHomeworkRfile.R
+++ ForkingHomeworkRfile.R
@@ -10,5 +10,9 @@ diamonds%>%
   filter(price > 7000 & price < 10000)%>%
   arrange(desc(carat))%>%
   slice(1:20)
-getwd()
-setwd("/Users/juanbreebaart/Desktop")
+
+diamonds <- as.data.table(diamonds)
+
+diamonds1 <- diamonds[, .(carat, cut, price)][cut == "Premium"][price > 7000 & price < 10000][order(-carat)]
+head(diamonds1, 20)
+
